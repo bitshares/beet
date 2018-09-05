3de303b1a699da63627e889d2cb64810682040c3
Alex Megalokonomos <alex@clockwork.gr>
2018-09-05 09:29:56 +0300

    Removed dummy DE translation post-testing

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -2,11 +2,14 @@ let localelist = [{
     iso: 'en',
     name: 'English',
     dir: 'ltr',
-}, {
-    iso: 'de',
-    name: 'Deutsch',
-    dir: 'ltr',
-}];
+}
+//,
+// {
+//    iso: 'de',
+//    name: 'Deutsch',
+//    dir: 'ltr',
+//}
+];
 
 export const locales=localelist;
 export const defaultLocale = localelist[0];
