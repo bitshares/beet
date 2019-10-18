285c458c05be891f1656e6c9f3598d926d3d8a50
Clockwork <alex@clockwork.gr>
2019-10-19 01:00:21 +0300

    Build Process update Tests

--- publish.sh
+++ publish.sh
@@ -1,9 +1,23 @@
+#!/bin/sh
 cd $TRAVIS_BUILD_DIR
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 
-## publish the binaries on tag
-if [ $TRAVIS_TAG ]
-then
-    npm run release
+if [[ "$TRAVIS_COMMIT_MESSAGE" =~ ^[\ []Release[]\ ].*$ ]]; then
+    export TRAVIS_TAG="v${PACKAGE_VERSION}"
+    export RELEASE_NAME="Beet Installer ${TRAVIS_TAG}"
+    export RELEASE_DESC="This is the official Beet v${PACKAGE_VERSION} release."
+fi
+if [[ "$TRAVIS_COMMIT_MESSAGE" =~ ^[\ []RC[]\ ].*$ ]]; then
+    export TRAVIS_TAG="v${PACKAGE_VERSION}-rc"
+    export RELEASE_NAME="Beet Installer ${TRAVIS_TAG}"
+    export RELEASE_DESC="This is a Release Candidate Beet build. Contains new features but may also contain bugs."
+fi
+if [[ "$TRAVIS_COMMIT_MESSAGE" =~ ^[\ []Test[]\ ].*$ ]]; then
+    export TRAVIS_TAG="v${PACKAGE_VERSION}-devbuild"
+    export RELEASE_NAME="Beet Installer ${TRAVIS_TAG}"
+    export RELEASE_DESC="This is a test development Beet build. Not for production use."
 fi
 
-ls -la dist
+git tag $TRAVIS_TAG
+git push --tags
