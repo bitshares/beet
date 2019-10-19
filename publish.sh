1b01fc7f712d38b92d169fc836684417d6211117
Clockwork <alex@clockwork.gr>
2019-10-19 08:28:52 +0300

    [RC] Test everything

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 cd $TRAVIS_BUILD_DIR
+PACKAGE_VERSION=$(node -p "require('./package.json').version")
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
